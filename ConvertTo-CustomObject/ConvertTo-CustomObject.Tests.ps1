e36fbb112046cd0108f2d5a2674c7af5a352b35a
Toffe Bergström <toffe@kabtoffe.net>
2017-07-12 16:23:54 +0300

    Wrapper function for Select-Object

---
+++ ConvertTo-CustomObject/ConvertTo-CustomObject.Tests.ps1
@@ -0,0 +1,46 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "ConvertTo-CustomObject" {
+    
+    $SourceObject = New-Object psobject -Property @{
+        "name" = "Test"
+        "age" = 75
+    }
+
+    $MappingTable = @{
+        "name" = "NewName"
+        "age" = "NewAge"
+    } 
+    
+    
+    It "Convert OK" {
+        $TargetObject = $SourceObject | ConvertTo-CustomObject -MappingTable $MappingTable
+        $TargetObject.NewName | Should Be "Test"
+        $TargetObject.NewAge | Should Be 75
+        $TargetObject.name | Should Be $null
+    }
+
+    $MappingTable.Add(
+        "NonExistantProperty",
+        "NewProperty"
+    )
+
+    It "Non existant property" {
+        $TargetObject = $SourceObject | ConvertTo-CustomObject -MappingTable $MappingTable
+        $TargetObject.NewName | Should Be "Test"
+        $TargetObject.NewAge | Should Be 75
+        $TargetObject.name | Should Be $null
+        $TargetObject.NewProperty | Should Be $null
+    }
+
+    It "Multiple objects" {
+        $result = $SourceObject,(New-Object psobject -Property @{ "age" = 7 }) | ConvertTo-CustomObject -MappingTable $MappingTable
+
+        $result.Count | Should Be 2
+        $result[1].NewAge | Should Be 7
+    }
+
+    It 
+}
---
+++ ConvertTo-CustomObject/ConvertTo-CustomObject.ps1
@@ -0,0 +1,34 @@
+﻿function ConvertTo-CustomObject{
+    [CmdletBinding()]
+
+    param(
+        #Input the object to transform
+        [Parameter(Mandatory=$True,ValueFromPipeline=$True)]
+        $SourceObject,
+
+        #Column mappings
+        [Parameter(Mandatory=$True)]
+        [hashtable]$MappingTable
+    )
+
+    BEGIN {
+        $PropertyMappingExpression = ""
+        $MappingTable.Keys | ForEach-Object {
+
+            $PropertyMappingExpression += (
+                @"
+                    @{
+                        N="$($MappingTable[$_])"
+                        E={`$_.$_}
+                    },
+"@
+)
+                    }
+        $PropertyMappingExpression = $PropertyMappingExpression.Substring(0,$PropertyMappingExpression.Length-1)
+        $SelectObjectParameter = Invoke-Expression $PropertyMappingExpression
+    }
+
+    PROCESS {
+        $SourceObject | Select-Object -Property $SelectObjectParameter
+    }    
+}
